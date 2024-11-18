2a389505cd0380548a377ad6eaa54a4c8a34ef59
alexforrepos <alexforrepos@gmail.com>
2024-11-18 19:50:29 +0300

    пример с текстурой и квадратом

--- OOP-Cursach/Source.cpp
+++ OOP-Cursach/Source.cpp
@@ -1,13 +1,22 @@
 #include <SFML/Graphics.hpp>
+#include <SFML/Network.hpp>
 using namespace std;
 using namespace sf;
 
 int main()
 {
-	RenderWindow win(VideoMode::VideoMode(600, 600), "Tempale");
+	RenderWindow win(VideoMode::VideoMode(1200, 1200), "Tempale");
 	Event ev;
+
+	RectangleShape r(Vector2f(200, 200));
+	r.setPosition(100, 100);
+	r.setFillColor(sf::Color::Cyan);
+	Texture t;
+	t.loadFromFile("pmg.png");
+	r.setTexture(&t, (200, 200));
 	while (win.isOpen())
 	{
+		win.clear();
 		while (win.pollEvent(ev))
 		{
 			switch (ev.type)
@@ -19,6 +28,15 @@ int main()
 				break;
 			}
 		}
-
+		if (Keyboard::isKeyPressed(Keyboard::D))
+			r.move(Vector2f(1, 0));
+		if (Keyboard::isKeyPressed(Keyboard::A))
+			r.move(Vector2f(-1, 0));
+		if (Keyboard::isKeyPressed(Keyboard::W))
+			r.move(0, -1);
+		if (Keyboard::isKeyPressed(Keyboard::S))
+			r.move(0, 1);
+		win.draw(r);
+		win.display();
 	}
 }
