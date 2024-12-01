d4ba75f80a53f208c946fc078657280b4b3f911c
alexforrepos <alexforrepos@gmail.com>
2024-12-01 09:53:37 +0300

    немного поменял архитектуру

---
+++ Server_/Server.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <SFML/Network.hpp>
+#include <string>
+
+
+
+class Server
+{
+	std::string adres;
+	
+};
+
--- Server_/main.cpp
+++ Server_/main.cpp
@@ -1,24 +1,4 @@
-﻿#include <SFML/Graphics.hpp>
-using namespace std;
-using namespace sf;
-
-int main()
+﻿int main()
 {
-	RenderWindow win(VideoMode::VideoMode(600, 600), "Tempale");
-	Event ev;
-	while (win.isOpen())
-	{
-		while (win.pollEvent(ev))
-		{
-			switch (ev.type)
-			{
-			case Event::Closed:
-				win.close();
-				break;
-			default:
-				break;
-			}
-		}	
 	
-	}
 }
