0c9b829df6c3cca7074c0106d3c00e11a665eb54
alexforrepos <alexforrepos@gmail.com>
2024-12-13 18:33:23 +0300

    еще классы

--- OOP-Cursach/EnemyManager.h
+++ OOP-Cursach/EnemyManager.h
@@ -1,10 +1,17 @@
 #pragma once
+
+enum ZOMBE_TO_SPAWN_SCORES
+{
+	NORMAL_ZOMBIE_POINT = 100,
+	ZOMBIE_WITH_TRAFFIC_CONE_POINT = 200,
+	ZOMBIE_WITH_IRON_BUCKET_POINT = 300,
+	ZOMBIE_GYMNAST_POINT = 300
+};
+
 class EnemyManager
 {
 	int points;
-
-
 public:
 	void Spawn();
-	void 
+	void GetPoints();
 };
--- OOP-Cursach/Zombie.h
+++ OOP-Cursach/Zombie.h
@@ -1,5 +1,6 @@
 #pragma once
 class Zombie
 {
+	int surface_level = 0;
 };
 
