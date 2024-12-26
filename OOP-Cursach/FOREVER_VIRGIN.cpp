c43fec1867a815b143d10453261e97c43f786dd0
F1nii <f1scnd@gmail.com>
2024-12-26 23:39:28 +0300

    abydabi

--- OOP-Cursach/FOREVER_VIRGIN.cpp
+++ OOP-Cursach/FOREVER_VIRGIN.cpp
@@ -2,21 +2,35 @@
 
 FOREVER_VIRGIN::FOREVER_VIRGIN()
 {
+	Position = sf::Vector2f(sf::Mouse::getPosition());
 	NEVER = nullptr;
+	spr.setTexture(*Res_Manager::getmger()->get_access<sf::Texture*>("sunflower.png"));//вставка текстуры
+	spr.setPosition(this->Position);//позиция картинки
 }
 
 void FOREVER_VIRGIN::Update()
 {
-	Position = sf::Vector2f(sf::Mouse::getPosition());
-	if (sf::Mouse::isButtonPressed(sf::Mouse::Left))
+	if (sf::Mouse::isButtonPressed(sf::Mouse::Left) && NEVER == nullptr)
+	{
 		NEVER = new LOOSE_VIRGIN();
+		O_Manager::getmger()->add_obj(NEVER);
+	}
+	if (NEVER != nullptr)
+	{
+		MSG_Manager::getmger()->add(new MSG(MSG_TYPE_MOVE(Position - sf::Vector2f(sf::Mouse::getPosition()), NEVER)));
+	}
+	Position = sf::Vector2f(sf::Mouse::getPosition());
+	if (NEVER && !sf::Mouse::isButtonPressed(sf::Mouse::Left))
+	{
+		MSG_Manager::getmger()->add(new MSG(MSG_TYPE_KILL(NEVER, this)));
+		NEVER = nullptr;
+	}
 }
 
 void FOREVER_VIRGIN::SendMSG(MSG* msg)
 {
 	switch (msg->MSG_TYPE.index())
 	{
-	case:
 	default:
 		break;
 	}
--- OOP-Cursach/FOREVER_VIRGIN.h
+++ OOP-Cursach/FOREVER_VIRGIN.h
@@ -3,12 +3,14 @@
 #include "MSG_Manager.h"
 #include "ResourceManager.h"
 #include "LOOSE_VIRGIN.h"
+#include "O_Manager.h"
 
 class FOREVER_VIRGIN
 	:public Object
 {
 private:
 	LOOSE_VIRGIN* NEVER;
+	sf::Sprite spr;
 public:
 
 	FOREVER_VIRGIN();
