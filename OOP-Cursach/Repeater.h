7f4205c648b312acd55507f624bef02a81249cd0
F1nii <f1scnd@gmail.com>
2025-02-24 19:08:01 +0300

    не ебу че тут

--- OOP-Cursach/Repeater.h
+++ OOP-Cursach/Repeater.h
@@ -10,10 +10,11 @@ public:
 		:Attack_Plants  (pos), time_to_repeat(2400)
 	{
 		sprite.setTexture(Res_Manager::getmger()->get_access<sf::Texture>("ps.png"));
+		time_to_repeat.restart();
+		time_to_shoot.restart(); 
 	}
 	void Update() override;
 	void SendMSG(MSG* msg) override;
 	void Draw(sf::RenderWindow& win) override;
-	//û
 };
 
