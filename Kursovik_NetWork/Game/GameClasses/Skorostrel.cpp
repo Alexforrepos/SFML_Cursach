0e42cd7b2b0c714681269f31ef2d624b119d5ec0
Keankup <maksl021@bk.ru>
2025-04-13 10:43:36 +0300

    чиню дальше

--- Kursovik_NetWork/Game/GameClasses/Skorostrel.cpp
+++ Kursovik_NetWork/Game/GameClasses/Skorostrel.cpp
@@ -12,7 +12,7 @@ Skorostrel::Skorostrel(const std::string& plantType, sf::Vector2f pos)
     clickTimer.restart();
 }
 
-// Реализация всех методов
+
 void Skorostrel::update()
 {
     sf::Vector2i mousePos = sf::Mouse::getPosition(Game::get().getWindow());
@@ -21,7 +21,7 @@ void Skorostrel::update()
 
     if (sf::Mouse::isButtonPressed(sf::Mouse::Left) && clickTimer())
     {
-        MSG_Manager::get().addMSG(std::make_shared<MSG_TYPE_KILL>(this, nullptr));
+        MSG_Manager::get().addMSG(std::make_shared<MSG_TYPE_KILL>(this, this));
         clickTimer.restart();
     }
 }
