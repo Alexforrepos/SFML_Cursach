c71a7203583593db87f2a6120b704a543d5d1663
Keankup <maksl021@bk.ru>
2025-05-02 20:44:51 +0300

    понс

---
+++ Engine/GameClasses/PeaShooter.cpp
@@ -0,0 +1,38 @@
+#include "PeaShooter.h"
+
+
+PeaShooter::PeaShooter(const std::string& plantType, uint8_t line, uint8_t col)
+    : Plant(line, col, Config::getInstance()["PlantParams"]["Plants"]["PeaShooter"]["HP"]),
+    plantType(plantType), shootTimer(Config::getInstance()["PlantParams"]["Plants"]["PeaShooter"]["CD"])
+{
+    sprite.setTexture(R_Manager::get().access<sf::Texture>("sun.png"));
+    sprite.setScale(0.15f, 0.15f);
+    sprite.setColor(sf::Color::White);
+    shootTimer.restart();
+}
+
+void PeaShooter::update() {
+    // Тут можешь реализовать логику стрельбы
+}
+
+void PeaShooter::sendMsg(Engine::MSG* msg) {}
+
+void PeaShooter::draw(sf::RenderWindow& win) {
+    win.draw(sprite);
+}
+
+sf::Vector2f PeaShooter::getPos() {
+    return sprite.getPosition();
+}
+
+void PeaShooter::changePos(const sf::Vector2f& other) {
+    setPos(getPos() + other);
+}
+
+void PeaShooter::setPos(sf::Vector2f other) {
+    sprite.setPosition(other);
+}
+
+
+CEREAL_REGISTER_TYPE(PeaShooter);
+CEREAL_REGISTER_POLYMORPHIC_RELATION(Plant, PeaShooter);
