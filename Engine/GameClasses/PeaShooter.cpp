48267c3d868a37e507eeb91b001b82d5e1fc97d9
Keankup <maksl021@bk.ru>
2025-05-10 18:44:11 +0300

    краш тест

--- Engine/GameClasses/PeaShooter.cpp
+++ Engine/GameClasses/PeaShooter.cpp
@@ -63,33 +63,33 @@ void PeaShooter::setPos(sf::Vector2f other) {
 	sprite.setPosition(other);
 }
 
-template <class Archive>
-void serialize(Archive& ar) {
-	// Сначала базовая Plant (line, col, HP)
-	ar(cereal::base_class<Plant>(this));
-	// Сохраняем свой plantType и состояние таймера
-	ar(plantType, shootTimer);
-
-	// Сериализуем визуальные параметры спрайта
-	sf::Vector2f position = this->sprite.getPosition();
-	sf::Vector2f scale = this->sprite.getScale();
-	float          rotation = this->sprite.getRotation();
-	sf::IntRect    textureRect = this->sprite.getTextureRect();
-	sf::Color      color = this->sprite.getColor();
-	// Тот же идентификатор, что вы используете для загрузки
-	std::string    textureId = "PeaShooter.png";
-
-	ar(position, scale, rotation, textureRect, color, textureId);
-
-	if constexpr (Archive::is_loading::value) {
-		// Восстанавливаем спрайт
-		sprite.setPosition(position);
-		sprite.setScale(scale);
-		sprite.setRotation(rotation);
-		sprite.setTextureRect(textureRect);
-		sprite.setColor(color);
-		sprite.setTexture(R_Manager::get().access<sf::Texture>(textureId));
-	}
-}
+//template <class Archive>
+//void serialize(Archive& ar) {
+//	// Сначала базовая Plant (line, col, HP)
+//	ar(cereal::base_class<Plant>(this));
+//	// Сохраняем свой plantType и состояние таймера
+//	ar(plantType, shootTimer);
+//
+//	// Сериализуем визуальные параметры спрайта
+//	sf::Vector2f position = this->sprite.getPosition();
+//	sf::Vector2f scale = this->sprite.getScale();
+//	float          rotation = this->sprite.getRotation();
+//	sf::IntRect    textureRect = this->sprite.getTextureRect();
+//	sf::Color      color = this->sprite.getColor();
+//	// Тот же идентификатор, что вы используете для загрузки
+//	std::string    textureId = "PeaShooter.png";
+//
+//	ar(position, scale, rotation, textureRect, color, textureId);
+//
+//	if constexpr (Archive::is_loading::value) {
+//		// Восстанавливаем спрайт
+//		sprite.setPosition(position);
+//		sprite.setScale(scale);
+//		sprite.setRotation(rotation);
+//		sprite.setTextureRect(textureRect);
+//		sprite.setColor(color);
+//		sprite.setTexture(R_Manager::get().access<sf::Texture>(textureId));
+//	}
+//}
 CEREAL_REGISTER_TYPE(PeaShooter);
 CEREAL_REGISTER_POLYMORPHIC_RELATION(Plant, PeaShooter);
